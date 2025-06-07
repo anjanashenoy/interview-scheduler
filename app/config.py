f8c40e4c4fb4044665d3a857c8562bad9aafaee1
Anjana Shenoy <anjana@Anjanas-MacBook-Air.local>
2025-06-07 10:12:09 -0400

    updates

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://interview_scheduler_dev_user:h3GeM3QwCUh6VoNJbLfmRSHnQY3h4rY0@dpg-d0ektbqdbo4c738plosg-a.virginia-postgres.render.com/interview_scheduler_dev'
+SQLALCHEMY_DATABASE_URI = 'postgresql://interview_scheduler_dev_lrvq_user:Ga2evrjcT0qdPCgs6lPJ9zTWhwXfvbJj@dpg-d1244cemcj7s73ev5020-a.virginia-postgres.render.com/interview_scheduler_dev_lrvq'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'hbkq2t3g80h2t4owges'
